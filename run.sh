f61a4536726e08f64b49dea48974c0ded8472703
Paul PHAM DANG <paul.phamdang@laposte.net>
2025-01-12 20:02:20 +0100

    Add script to run the whole simulation

---
+++ run.sh
@@ -0,0 +1,13 @@
+cd build
+# Cleaning the previous compiled executables if present
+make clean
+# Generating make file
+cmake ..
+# Compiling
+make
+# Going inside the folder with the executables
+cd ../bin
+# remove log file
+rm ./log/log.log
+# Running the project
+./master
