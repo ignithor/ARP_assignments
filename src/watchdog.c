547f6f4cb9e30cc51a0ed080f27775e24f3ddcad
yuimomi <yuimomi0513@gmail.com>
2025-02-09 12:33:28 +0100

    Refactor: Clean up code in watchdog for better readability

--- src/watchdog.c
+++ src/watchdog.c
@@ -3,161 +3,143 @@
 #include "wrappers/wrappers.h"
 #include <time.h>
 
-// Array for processes' pids
+// Array to store process PIDs
 int p_pids[NUM_PROCESSES];
 
-// Pid of the konsole executing input and the map processes
-int pid_konsole_input, pid_konsole_map;
+// PIDs of konsole processes executing Input and Map
+int konsole_input_pid	, konsole_map_pid	;
 
-// Count to check whether process has replied to WD
-int count = 0;
+// Counter to track responses from processes to WD
+int response_count = 0;
 
-// Check of kill
-int check;
+// Check variable for kill status
+int kill_status	;
 
-// Pid of the dead process
+// PID of the detected faulty process
 int fault_pid;
 
-// Signal handler for signals received from the monitored processes
-void signal_handler_wd(int signo, siginfo_t *info, void *context) {
-    // Specifying that context and info are unused
+/**
+ * Signal handler for SIGUSR2 signals received from monitored processes.
+ * This indicates that a process has responded to the watchdog.
+ */
+void watchdog_signal_handler(int signo, siginfo_t *info, void *context) {
+    // Marking context and info as unused to avoid compiler warnings
     (void)(info);
     (void)(context);
 
-    // A process responded to SIGUSR1 so increase count
+    // If SIGUSR2 is received, increment the response count
     if (signo == SIGUSR2)
-        count++;
+        response_count++;
 }
 
+
 int main(int argc, char *argv[]) {
-    // Signal declaration
+    // Initialize and configure signal handler
     struct sigaction sa;
     memset(&sa, 0, sizeof(sa));
 
-    // Setting the signal handler
-    sa.sa_sigaction = signal_handler_wd;
+    sa.sa_sigaction = watchdog_signal_handler; // Assign signal handler function
     sigemptyset(&sa.sa_mask);
-    // Setting flags
-    // The SA_RESTART flag is used to restart all those syscalls that can get
-    // interrupted by signals
+    
+    // SA_RESTART ensures interrupted system calls are restarted
     sa.sa_flags = SA_SIGINFO | SA_RESTART;
 
-    // Enabling the handler with the specified flags
+    // Register signal handler for SIGUSR2
     Sigaction(SIGUSR2, &sa, NULL);
 
-    // argv[1] -> server
-    // argv[2] -> drone
-    // argv[3] -> konsole of input
-    // argv[4] -> konsole of map
-    // argv[5] -> target
-    // argv[6] -> obstacles
+    // Verify the correct number of arguments
     if (argc == NUM_PROCESSES) {
-        sscanf(argv[1], "%d", &p_pids[2]);
-        sscanf(argv[2], "%d", &p_pids[3]);
-        sscanf(argv[3], "%d", &pid_konsole_input);
-        sscanf(argv[4], "%d", &pid_konsole_map);
-        sscanf(argv[5], "%d", &p_pids[4]);
-        sscanf(argv[6], "%d", &p_pids[5]);
+        sscanf(argv[1], "%d", &p_pids[2]);  // Server PID
+        sscanf(argv[2], "%d", &p_pids[3]);  // Drone PID
+        sscanf(argv[3], "%d", &konsole_input_pid	);  // Konsole running Input
+        sscanf(argv[4], "%d", &konsole_map_pid	);  // Konsole running Map
+        sscanf(argv[5], "%d", &p_pids[4]);  // Target PID
+        sscanf(argv[6], "%d", &p_pids[5]);  // Obstacles PID
     } else {
-        perror("arg_list error");
+        perror("Invalid argument list");
         exit(1);
     }
 
-    // Auxiliary variable used to create the log message to insert into the
-    // logfile
+    // Logging message buffer
     char logmsg[100];
 
-    // Getting the input pid through the named pipe
-    // Initializing the named pipe
+    // Retrieve Input process PID through a named pipe (FIFO)
     int fd1;
     Mkfifo(FIFO2_PATH, 0666);
 
-    // String to save the input pid
+    // Buffer to store the Input process PID
     char input_pid_str[10];
 
-    // Opening the named pipe and reading the input pid from it
+    // Read Input PID from FIFO
     fd1 = Open(FIFO2_PATH, O_RDONLY);
     Read(fd1, input_pid_str, sizeof(input_pid_str));
     Close(fd1);
 
-    // Saving the input pid in the array of processes pids
+    // Store the Input PID in the process array
     sscanf(input_pid_str, "%d", &p_pids[0]);
 
-    // Printing the received input pid to verify its correctness
-    printf("Input pid is %s\n", input_pid_str);
+    // Print received Input PID for verification
+    printf("Input PID: %s\n", input_pid_str);
 
-    // Getting both the map pid and the pid of the Konsole running map through
-    // the named pipe
-    // Initializing the named pipe
+    // Retrieve Map process PID and its Konsole PID through another named pipe
     int fd2;
     Mkfifo(FIFO1_PATH, 0666);
 
-    // String to save both pids
+    // Buffer to store Map process and Konsole PIDs
     char map_pids_str[20];
 
-    // Opening the named pipe and reading the pids from it
+    // Read Map process PID from FIFO
     fd2 = Open(FIFO1_PATH, O_RDONLY);
     Read(fd2, map_pids_str, sizeof(map_pids_str));
     Close(fd2);
 
-    // Save map pid in the array of processes pids and the pid of the Konsole
-    // running map in a separate int variable.
-    // This because the Konsole(s) do not receive nor send signals to the WD,
-    // but only need to be killed if a process freezes or dies
+    // Store Map process PID in the process array
     sscanf(map_pids_str, "%d", &p_pids[1]);
 
-    // Printing the received pids to verify their correctness
-    printf("Map pid is %d \n", p_pids[1]);
+    // Print received Map PID for verification
+    printf("Map PID: %d\n", p_pids[1]);
+
 
     while (1) {
-        // Iterate for the number of processes to check
+        // Iterate over all monitored processes (excluding watchdog itself)
         for (int i = 0; i < NUM_PROCESSES - 1; i++) {
-            // Saving the return value of the kill so that it is possible to
-            // immediately verify if the kill failed, so if a process died
-            check = Kill2(p_pids[i], SIGUSR1);
+            // Send SIGUSR1 signal to the process and store the return value
+            kill_status	 = Kill2(p_pids[i], SIGUSR1);
 
-            // Writing in the logfile which process the WD sent a signal to
-            sprintf(logmsg, "WD sending signal to %d", p_pids[i]);
+            // Log the signal being sent
+            sprintf(logmsg, "WD sending signal to process PID: %d", p_pids[i]);
             logging("INFO", logmsg);
 
-            // This may seem strange but is required because signals may
-            // interrupt sleep. Sleep when interrupted returns the amount of
-            // seconds for which it still needed to sleep for. So this is
-            // actually restarting the sleep in case it does not return 0. Now
-            // it may seem a busy wait but it's actually not because of how
-            // signals are handled
-            while (sleep(WD_SLEEP_PERIOD))
-                ;
+            // Handle interruptions in sleep caused by signals
+            // sleep() may return early due to a signal, so we retry until the sleep duration is met
+            while (sleep(WD_SLEEP_PERIOD));
 
-            // If either the process is dead, so the kill failed returning -1 to
-            // check, or the process is frozen, so it did not increment the
-            // count, the WD kills all processes and exits
-            if (check == -1 || count == 0) {
-                // Saving the pid of the dead/frozen process in a specific
-                // variable
+            // Check if the process is either dead (kill failed) or frozen (did not increment count)
+            if (kill_status	 == -1 || response_count == 0) {
+                // Save the PID of the failed process
                 fault_pid = p_pids[i];
 
-                sprintf(logmsg,
-                        "WD killed all processes because of process"
-                        " with pid %d",
+                // Log the failure and termination of all processes
+                sprintf(logmsg, 
+                        "WD detected failure in process PID: %d. Terminating all processes.", 
                         fault_pid);
                 logging("WARN", logmsg);
 
-                // Killing all processes, except Konsole's
+                // Kill all monitored processes (excluding Konsole processes)
                 for (int i = 0; i < NUM_PROCESSES - 1; i++) {
                     Kill2(p_pids[i], SIGKILL);
                 }
 
-                /// Killing Konsole's
-                Kill2(pid_konsole_input, SIGKILL);
-                Kill2(pid_konsole_map, SIGKILL);
+                // Kill Konsole processes separately
+                Kill2(konsole_input_pid	, SIGKILL);
+                Kill2(konsole_map_pid	, SIGKILL);
 
-                // Exiting with success
+                // Exit successfully after termination
                 return EXIT_SUCCESS;
             }
-            // if the process is not dead nor frozen, reset its count
-            else
-                count = 0;
+
+            // Reset count if the process responded correctly
+            response_count = 0;
         }
     }
     return EXIT_SUCCESS;
