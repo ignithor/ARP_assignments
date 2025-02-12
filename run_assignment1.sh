8a7a3b9a39c4265eb5231a43fc99a9ce0ea1ea4d
Paul PHAM DANG <paul.phamdang@laposte.net>
2025-02-12 15:38:30 +0100

    Improve running script

--- run_assignment1.sh
+++ run_assignment1.sh
@@ -1,21 +1,20 @@
+# Delete old build file if it exists
 if [ -d "build" ]; then
   rm -r build
 fi
 
-if [ -d "bin" ]; then
-  rm -r bin
-fi
-
-touch log/process.log
-> log/process.log
-
+# Create a new build folder and go into it
 mkdir build
 cd build
+
 # Generating make file
 cmake ..
+
 # Compiling
 make
-# Going inside the folder with the executables
+
+# Go inside the folder with all the executables
 cd ../bin
-# Running the project
+
+# Running the project by running the master process
 ./master
