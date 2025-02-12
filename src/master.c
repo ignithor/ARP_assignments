f42dc267e07fdcb639e97b159687f4c99b5e8c48
Paul PHAM DANG <paul.phamdang@laposte.net>
2025-02-12 15:32:28 +0100

    Add creat to create the log file

--- src/master.c
+++ src/master.c
@@ -15,6 +15,14 @@ int main(int argc, char *argv[]) {
     (void)(argv);
 
     // Define an array of strings for every process to spawn
+    int log_file = creat("../log/process.log", 0666);
+
+    if (log_file < 0) {
+        perror("Error creating log file");
+        exit(EXIT_FAILURE);
+    }
+    logging("INFO", "Beginning of the master process");
+
     char process_names[NUM_PROCESSES][20];
     strcpy(process_names[0], "./server");
     strcpy(process_names[1], "./drone");
@@ -71,15 +79,26 @@ int main(int argc, char *argv[]) {
         if (!child_pids[i]) {
 
             // Spawn the input and map process using konsole
-            char *exec_args[] = {process_names[i], NULL, NULL, NULL, NULL, NULL,
-                                NULL,        NULL, NULL, NULL, NULL, NULL};
-            char *konsole_arg_list[] = {"konsole", "-e", process_names[i], NULL,
-                                        NULL,      NULL, NULL};
+            char *exec_args[]        = {process_names[i],
+                                        NULL,
+                                        NULL,
+                                        NULL,
+                                        NULL,
+                                        NULL,
+                                        NULL,
+                                        NULL,
+                                        NULL,
+                                        NULL,
+                                        NULL,
+                                        NULL};
+            char *konsole_arg_list[] = {
+                "konsole", "-e", process_names[i], NULL, NULL, NULL, NULL};
 
             switch (i) {
                 case 0:
-                    // **Server Process Setup**       
-                    // Convert pipe file descriptors to strings for argument passing.
+                    // **Server Process Setup**
+                    // Convert pipe file descriptors to strings for argument
+                    // passing.
                     sprintf(drone_server_str, "%d", drone_server[0]);
                     sprintf(server_drone_str, "%d", server_drone[1]);
                     sprintf(input_server_str, "%d", input_server[0]);
@@ -104,8 +123,8 @@ int main(int argc, char *argv[]) {
                     exec_args[10] = server_obstacle_str;
 
                     // **Close unused pipe ends** to prevent resource leaks
-                    Close(drone_server[1]);   // Server only reads from drone
-                    Close(server_drone[0]);   // Server only writes to drone
+                    Close(drone_server[1]); // Server only reads from drone
+                    Close(server_drone[0]); // Server only writes to drone
                     Close(input_server[1]);
                     Close(server_input[0]);
                     Close(map_server[1]);
@@ -114,7 +133,7 @@ int main(int argc, char *argv[]) {
                     Close(server_target[0]);
                     Close(obstacle_server[1]);
                     Close(server_obstacle[0]);
-                    
+
                     // Spawn the server process
                     spawn(exec_args);
                     break;
@@ -129,8 +148,8 @@ int main(int argc, char *argv[]) {
                     exec_args[2] = drone_server_str;
 
                     // **Close unused pipe ends**
-                    Close(server_drone[1]);   // Drone only reads from server
-                    Close(drone_server[0]);   // Drone only writes to server
+                    Close(server_drone[1]); // Drone only reads from server
+                    Close(drone_server[0]); // Drone only writes to server
 
                     // Close all unrelated pipes to avoid interference
                     Close(server_input[0]);
@@ -165,16 +184,22 @@ int main(int argc, char *argv[]) {
                     konsole_arg_list[4] = server_input_str;
 
                     // **Close unused pipe ends** for input process
-                    Close(input_server[0]);  // Input only writes to the server
-                    Close(server_input[1]);  // Input only reads from the server
+                    Close(input_server[0]); // Input only writes to the server
+                    Close(server_input[1]); // Input only reads from the server
 
                     // Close all unrelated pipes
-                    Close(map_server[0]); Close(map_server[1]);
-                    Close(server_map[0]); Close(server_map[1]);
-                    Close(target_server[0]); Close(target_server[1]);
-                    Close(server_target[0]); Close(server_target[1]);
-                    Close(obstacle_server[0]); Close(obstacle_server[1]);
-                    Close(server_obstacle[0]); Close(server_obstacle[1]);
+                    Close(map_server[0]);
+                    Close(map_server[1]);
+                    Close(server_map[0]);
+                    Close(server_map[1]);
+                    Close(target_server[0]);
+                    Close(target_server[1]);
+                    Close(server_target[0]);
+                    Close(server_target[1]);
+                    Close(obstacle_server[0]);
+                    Close(obstacle_server[1]);
+                    Close(server_obstacle[0]);
+                    Close(server_obstacle[1]);
 
                     // Launch the input process in a new terminal window
                     Execvp("konsole", konsole_arg_list);
@@ -192,20 +217,24 @@ int main(int argc, char *argv[]) {
                     konsole_arg_list[4] = server_map_str;
 
                     // **Close unused pipe ends** for map process
-                    Close(map_server[0]);  // Map only writes to the server
-                    Close(server_map[1]);  // Map only reads from the server
+                    Close(map_server[0]); // Map only writes to the server
+                    Close(server_map[1]); // Map only reads from the server
 
                     // Close all unrelated pipes
-                    Close(target_server[0]); Close(target_server[1]);
-                    Close(server_target[0]); Close(server_target[1]);
-                    Close(obstacle_server[0]); Close(obstacle_server[1]);
-                    Close(server_obstacle[0]); Close(server_obstacle[1]);
+                    Close(target_server[0]);
+                    Close(target_server[1]);
+                    Close(server_target[0]);
+                    Close(server_target[1]);
+                    Close(obstacle_server[0]);
+                    Close(obstacle_server[1]);
+                    Close(server_obstacle[0]);
+                    Close(server_obstacle[1]);
 
                     // Launch the map process in a new terminal window
                     Execvp("konsole", konsole_arg_list);
                     exit(EXIT_FAILURE);
                     break;
-                
+
                 case 4:
                     // **Target Process Setup**
                     // Convert pipe descriptors to strings for argument passing
@@ -217,12 +246,15 @@ int main(int argc, char *argv[]) {
                     exec_args[2] = server_target_str;
 
                     // **Close unused pipe ends** for the target process
-                    Close(target_server[0]);  // Target only writes to the server
-                    Close(server_target[1]);  // Target only reads from the server
+                    Close(target_server[0]); // Target only writes to the server
+                    Close(
+                        server_target[1]); // Target only reads from the server
 
                     // Close all unrelated pipes (Obstacle-related)
-                    Close(obstacle_server[0]); Close(obstacle_server[1]);
-                    Close(server_obstacle[0]); Close(server_obstacle[1]);
+                    Close(obstacle_server[0]);
+                    Close(obstacle_server[1]);
+                    Close(server_obstacle[0]);
+                    Close(server_obstacle[1]);
 
                     // Spawn the target process
                     spawn(exec_args);
@@ -239,55 +271,68 @@ int main(int argc, char *argv[]) {
                     exec_args[2] = server_obstacle_str;
 
                     // **Close unused pipe ends** for the obstacle process
-                    Close(obstacle_server[0]);  // Obstacle only writes to the server
-                    Close(server_obstacle[1]);  // Obstacle only reads from the server
+                    Close(obstacle_server[0]); // Obstacle only writes to the
+                                               // server
+                    Close(server_obstacle[1]); // Obstacle only reads from the
+                                               // server
 
                     // Spawn the obstacle process
                     spawn(exec_args);
                     break;
-             }
-            //  Spawn the last process: Watchdog (WD), which monitors all other processes
+            }
+            //  Spawn the last process: Watchdog (WD), which monitors all other
+            //  processes
             if (i == NUM_PROCESSES - 1) {
                 for (int i = 0; i < NUM_PROCESSES - 1; i++)
                     sprintf(child_pids_str[i], "%d", child_pids[i]);
 
                 // Sending as arguments to the WD all the processes PIDs
-                char *exec_args[] = {process_names[i],       child_pids_str[0],
-                                    child_pids_str[1], child_pids_str[2],
-                                    child_pids_str[3], child_pids_str[4],
-                                    child_pids_str[5], NULL};
+                char *exec_args[] = {process_names[i],  child_pids_str[0],
+                                     child_pids_str[1], child_pids_str[2],
+                                     child_pids_str[3], child_pids_str[4],
+                                     child_pids_str[5], NULL};
                 spawn(exec_args);
             }
         } else {
             // **Parent Process: Close unused pipes**
             // Since pipes are duplicated for each fork, close unnecessary ones
             switch (i) {
-                    case 1: // **Drone has spawned**
-                        Close(server_drone[0]); Close(server_drone[1]);
-                        Close(drone_server[0]); Close(drone_server[1]);
-                        break;
+                case 1: // **Drone has spawned**
+                    Close(server_drone[0]);
+                    Close(server_drone[1]);
+                    Close(drone_server[0]);
+                    Close(drone_server[1]);
+                    break;
 
-                    case 2: // **Input has spawned**
-                        Close(server_input[0]); Close(server_input[1]);
-                        Close(input_server[0]); Close(input_server[1]);
-                        break;
+                case 2: // **Input has spawned**
+                    Close(server_input[0]);
+                    Close(server_input[1]);
+                    Close(input_server[0]);
+                    Close(input_server[1]);
+                    break;
 
-                    case 3: // **Map has spawned**
-                        Close(server_map[0]); Close(server_map[1]);
-                        Close(map_server[0]); Close(map_server[1]);
-                        break;
+                case 3: // **Map has spawned**
+                    Close(server_map[0]);
+                    Close(server_map[1]);
+                    Close(map_server[0]);
+                    Close(map_server[1]);
+                    break;
 
-                    case 4: // **Target has spawned**
-                        Close(target_server[0]); Close(target_server[1]);
-                        Close(server_target[0]); Close(server_target[1]);
-                        break;
+                case 4: // **Target has spawned**
+                    Close(target_server[0]);
+                    Close(target_server[1]);
+                    Close(server_target[0]);
+                    Close(server_target[1]);
+                    break;
 
-                    case 5: // **Obstacle has spawned**
-                        Close(obstacle_server[0]); Close(obstacle_server[1]);
-                        Close(server_obstacle[0]); Close(server_obstacle[1]);
-                        break;
-                }        
-                }
+                case 5: // **Obstacle has spawned**
+                    Close(obstacle_server[0]);
+                    Close(obstacle_server[1]);
+                    Close(server_obstacle[0]);
+                    Close(server_obstacle[1]);
+                    break;
+            }
+        }
     }
 
     // Print PIDs of all spawned processes
@@ -302,16 +347,17 @@ int main(int argc, char *argv[]) {
     printf("---------------------\n\n");
 
     // Value for waiting for the children to terminate
-    int exit_status	;
+    int exit_status;
 
     // Retrieve and display the exit status of the terminated process
     for (int i = 0; i < NUM_PROCESSES; i++) {
-        int ret = Wait(&exit_status	);
+        int ret = Wait(&exit_status);
         // Getting the exit status
         int status = 0;
         WEXITSTATUS(status);
         printf("Process %d terminated with code: %d\n", ret, status);
     }
+    close(log_file);
 
     return EXIT_SUCCESS;
 }
